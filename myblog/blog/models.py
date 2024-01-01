31c44f5b5ad0c127b04e5df17c20935da1480e72
niolmo <71871013+niolmo@users.noreply.github.com>
2024-01-01 17:08:52 +0500

    blog 3

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -15,16 +15,18 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
-    title = models.CharField(max_length=250)
-    slug = models.SlugField("max_length=250")
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    slug = models.SlugField(
+        max_length=250, verbose_name='URL', unique_for_date='publ')
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='Посты')
-    text = models.TextField()
-    publ = models.DateTimeField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+        User, on_delete=models.CASCADE, related_name='Автор', verbose_name='Автор')
+    text = models.TextField(verbose_name='Текст')
+    publ = models.DateTimeField(
+        verbose_name='Опубликовано', default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     status = models.CharField(
-        max_length=2, choices=Status.choices, default=Status.DRAFT)
+        max_length=2, choices=Status.choices, default=Status.DRAFT, verbose_name='Статус')
 
     objects = models.Manager()  # Менеджер моделей по умолчанию
     published = PabMan()  # Менеджер моделей опубликованных постов
@@ -34,6 +36,8 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publ']),
         ]
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
     def __str__(self):
         return self.title
